7a11acf6b1c77694fd7a78923474e9c97520229b
srwa <srwa.gailan@motillo.com>
2020-01-01 14:03:03 +0100

    menu added some how but not precise

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Logo from './components/Logo';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> hi and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+  
+  container = React.createRef();
+  state = {
+    open: false,
+  };
+  handleButtonClick = () => {
+    this.setState(state => {
+      return {
+        open: !state.open,
+      };
+    });
+  };
+ 
+  handleClickOutside = event => {
+    if (this.container.current && !this.container.current.contains(event.target)) {
+      this.setState({
+        open: false,
+      });
+    }
+  };
+  componentDidMount() {
+    document.addEventListener("mousedown", this.handleClickOutside);
+}
+componentWillUnmount() {
+  document.removeEventListener("mousedown", this.handleClickOutside);
+}
+  render() {
+    return (
+      <div className="App">
+        <div className="container" ref={this.container}>
+            <button type="button" className="button" onClick={this.handleButtonClick}>
+            <Logo ></Logo>
+            </button>
+            {this.state.open && (
+            <div className="dropdown">
+              <ul>
+                <li>Option 1</li>
+                <li>Option 2</li>
+                <li>Option 3</li>
+                <li>Option 4</li>
+              </ul>
+            </div>
+          )}
+          </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Logo.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import earth from './earth.svg';
+const Logo= props => (
+    <img src={earth} className="" alt="logo" />
+ );
+ export default Logo;
